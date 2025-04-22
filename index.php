6bbbf36572b360cec8a1d40f665ba0d94c62d3d5
aelton-lima <aelton.lima1993@gmail.com>
2025-04-22 11:11:47 -0300

    Add Login na tela principal
    Adicionamos login na tela principal

--- index.php
+++ index.php
@@ -1,13 +1,34 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HOME</title>
+  <meta charset="UTF-8">
+  <title>Início | Tampets</title>
+  <link rel="stylesheet" href="css/index.css">
+   <!-- Adicionando Font Awesome -->
+   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
 </head>
 <body>
-    pagina inicial
-    
+  <nav>
+    <div class="logo">Tampets</div>
+    <ul>
+      <li><a href="index.html"><i class="fas fa-home"></i>Início</a></li>
+      <li><a href="sobre.html"><i class="fas fa-info-circle"></i>Sobre</a></li>
+      <li><a href= "pontos_coleta.html"><i class="fa fa-map-signs"></i>Pontos de Coleta</a></li>
+      <li><a href="ajudar.html"><i class="fas fa-hand-holding-heart"></i>Como Ajudar</a></li>
+      <li><a href="contato.html"><i class="fas fa-envelope"></i>Contato</a></li>
+      <li><a href="login.php"><i class="fas fa-envelope"></i>Login</a></li>
+    </ul>
+  </nav>
+  
+  <div class="container">
+    <h1>Bem-vindo à Tampets</h1>
+    <p>Nosso site está no ar 🚀 Cuidando de vidas com amor e tampinhas.</p>
+    <img src="assets/tampets.jpg" alt="Logo da Tampets" style="max-width: 100%; border-radius: 12px;">
+  </div>
+
+  <footer>
+    © 2025 Tampets Sorocaba. Todos os direitos reservados.
+  </footer>
 </body>
 </html>
 
